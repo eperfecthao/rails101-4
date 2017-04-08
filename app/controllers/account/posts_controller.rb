3f69eabc17baaff02844eb3fc6c75afbb292d974
songhao <songhao@songhaodeMacBook-Pro.local>
2017-04-08 15:36:01 +0800

    use simple_format helper to decorate

--- app/controllers/account/posts_controller.rb
+++ app/controllers/account/posts_controller.rb
@@ -4,5 +4,32 @@ class Account::PostsController < ApplicationController
   def index
     @posts = current_user.posts
   end
-  
+
+  def edit
+      @post = Post.find(params[:id])
+  end
+
+  def update
+      @post = Post.find(params[:id])
+    if current_user && current_user == @post.user
+      @post.update(post_params)
+
+      redirect_to account_posts_path, notice: "更新成功"
+    else
+      render root_path, alert: "You have no permission"
+    end
+  end
+
+  def destroy
+      @post = Post.find(params[:id])
+      @post.destroy
+      redirect_to account_posts_path, alert: "文章删除"
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:content)
+  end
+
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :find_post_and_check_permission, only: [:edit, :update, :destroy]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
   def new
     @group = Group.find(params[:group_id])
@@ -20,11 +21,39 @@ class PostsController < ApplicationController
 
   end
 
+  def edit
+    @group = Group.find(params[:group_id])
+  end
+
+  def update
+    @group = Group.find(params[:group_id])
+    if @post.update(post_params)
+      redirect_to account_posts_path, notice: "更新成功"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @post.destroy
+      redirect_to account_posts_path, alert: "删除成功"
+  end
+
+
 
   private
 
   def post_params
     params.require(:post).permit(:content)
+  end
+
+  def find_post_and_check_permission
+
+    @post = Post.find(params[:id])
+
+    if current_user != @post.user
+      redirect_to root_path, alert: "You have no permission"
+    end
   end
 
 end
